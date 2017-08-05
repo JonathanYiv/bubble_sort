13cea5757054e3a0e43b424a069f284076c25c2b
Jonathan Yiv <JonathanYiv@gmail.com>
2017-08-04 21:59:04 -0400

    Completed both bubble sorts

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,16 +1,37 @@
 def bubble_sort(array)
-
+	sorted = false
+	while sorted == false
+		temp = array.clone
+		array.each_index { |i| array[i], array[i + 1] = array[i + 1], array[i] if i < array.length - 1 && array[i] > array[i + 1] }
+		sorted = true if temp == array
+	end
+	array
 end
 
 puts bubble_sort([4,3,78,2,0,2])
 # [0,2,2,4,78]
 
 
+
 def bubble_sort_by(array)
-	yield(argument1, argument2)
+	sorted = false
+	while sorted == false
+		temp = array.clone
+		array.each_index do |i|
+			if i < array.length - 1
+				if yield(array[i], array[i+1]) > 0
+					array[i], array[i+1] = array[i+1], array[i]
+				end
+			end
+		end
+		sorted = true if temp == array
+	end
+	array
 end
 
-bubble_sort_by(["hi", "hello", "hey"]) do |left,right|
+solution = bubble_sort_by(["hi", "hello", "hey"]) do |left,right|
 	left.length - right.length
 end
 # ["hi", "hey", "hello"]
+
+puts solution
